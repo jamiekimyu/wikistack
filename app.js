dd7f65c1d6dc1cff96a53eec17ea26f9e0ef2a0f
Jason PowellE <jpdevelopment@icloud.com>
2017-01-23 11:16:50 -0500

    3. Virtual Fields

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var models = require('./models');
+var nunjucks = require('nunjucks');
+var env = nunjucks.configure('./views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.then(function () {
+    server.listen(9001, function () {
+        console.log("It's over 9000!");
+    });
+})
+.catch(console.error);
