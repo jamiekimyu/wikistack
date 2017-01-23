247817205b7c2a54896ad7df9f1a17518188d7a7
Jamie Yu <jme.kim.yu@gmail.com>
2017-01-23 15:47:50 -0500

    finished hooks

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,24 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var Page = models.Page;
+var User = models.User;
 
 module.exports = router;
 
 router.get('/', function(req, res, next){
-    res.redirect('/');
+    res.send('this is the homepage');
+    //res.redirect('/');
 })
 
 router.post('/', function(req, res, next){
-    res.send('post works');
+    //res.send('post works');
+    var pageTitle = req.body.pageTitle;
+    var pageContent = req.body.pageContent;
     console.log(req.body);
-//     var page = Page.build({
-//     title: ???????,
-//     content: ???????
-//   });
-    //  page.save();
-    //  res.redirect('/');
+
+    var page = Page.build({
+    title: pageTitle,
+    content: pageContent
+  });
+
+     page.save().then(function(page){
+         res.redirect(page.route);
+     });
+     
 })
 
 router.get('/add', function(req, res, next){
     res.render('addpage');
+})
+
+router.get('/:pageTitle', function(req, res, next){
+    res.send('new page');
 })
